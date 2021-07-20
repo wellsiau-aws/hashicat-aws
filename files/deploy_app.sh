3d1b4f6103fc6d66ce835ec3d0c38e188a0d7f98
Welly Siauw AWS <67753754+wellsiau-aws@users.noreply.github.com>
2021-07-20 12:21:19 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to my cat app.
+  Welcome to my cat app. ver 2
   <!-- END -->
 
   </div>
